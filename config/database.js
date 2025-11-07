58618b841545bb2c4a8c0a87ceaf74087c3c5266
2023200146-beep <2023200146@ucss.pe>
2025-11-06 21:43:08 -0500

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -11,26 +11,13 @@ const dbConfig = {
     }
 };
 
-console.log('Configuración BD:', {
-    host: dbConfig.host,
-    user: dbConfig.user,
-    database: dbConfig.database,
-    port: dbConfig.port
-});
-
 const db = mysql.createConnection(dbConfig);
 
 const queryDB = (sql, params = []) => {
-    console.log('Ejecutando query:', sql, 'Params:', params);
     return new Promise((resolve, reject) => {
         db.query(sql, params, (err, results) => {
-            if (err) {
-                console.error('Error en query:', err.message);
-                reject(err);
-            } else {
-                console.log('Query exitosa. Resultados:', results.length);
-                resolve(results);
-            }
+            if (err) reject(err);
+            else resolve(results);
         });
     });
 };
@@ -38,10 +25,9 @@ const queryDB = (sql, params = []) => {
 db.connect((err) => {
     if (err) {
         console.error('Error conectando a la BD:', err.message);
-        console.error('Detalles del error:', err);
         return;
     }
-    console.log('Conectado a la base de datos MySQL en producción');
+    console.log('Conectado a la base de datos MySQL');
 });
 
 module.exports = { db, queryDB };
